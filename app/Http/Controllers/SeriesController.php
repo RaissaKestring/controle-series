b80d56f6e281005cd5c1b7b0adbe7358b453d13e
RaissaKestring <raissakestring23@gmail.com>
2024-05-14 15:04:30 -0300

    Conclusão do Curso

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,23 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request) {
 
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
-
-        compact(var_name: 'series');
+        $series = Serie::query()->orderBy(column:'nome')->get();
         return view('series.index')->with('series', $series);
     }
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request, Serie $serie) {
+        $nomeSerie=$request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+        return redirect('/series');
     }
 }
